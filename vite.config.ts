068553ae1977f96c4fea5d769b8a99c2c0559795
Eva Decker <eva@namesake.fyi>
2024-09-07 17:40:26 -0400

    chore: Add coverage reporting (#32)

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,12 @@
+/// <reference types="vitest" />
+
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import react from "@vitejs/plugin-react-swc";
 import autoprefixer from "autoprefixer";
 import cssnano from "cssnano";
 import tailwindcss from "tailwindcss";
 import { defineConfig } from "vite";
+import { configDefaults, coverageConfigDefaults } from "vitest/config";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -14,6 +17,26 @@ export default defineConfig({
   css: {
     postcss: {
       plugins: [autoprefixer(), tailwindcss(), cssnano()],
+    },
+  },
+  test: {
+    environment: "edge-runtime",
+    environmentMatchGlobs: [
+      ["convex/**", "edge-runtime"],
+      ["**", "jsdom"],
+    ],
+    server: { deps: { inline: ["convex-test"] } },
+    exclude: [...configDefaults.exclude],
+    coverage: {
+      reporter: ["text", "json-summary", "json"],
+      reportOnFailure: true,
+      include: ["src/**", "convex/**"],
+      exclude: [
+        ...coverageConfigDefaults.exclude,
+        "**/*.config.?(c|m)[jt]s?(x)",
+        "convex/_generated/**",
+        "src/routeTree.gen.ts",
+      ],
     },
   },
 });
