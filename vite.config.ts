1a3ecadaf6a0d6ddb3b486540e3a35a1a4c1fd0b
Eva Decker <itsevadecker@gmail.com>
2024-09-07 17:15:40 -0400

    Add vitest reference types

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest" />
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import react from "@vitejs/plugin-react-swc";
 import autoprefixer from "autoprefixer";
