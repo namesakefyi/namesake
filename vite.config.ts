24c61e4bcdbb8a1e27cad8ac040a79621f35861d
Eva Decker <itsevadecker@gmail.com>
2024-09-07 16:47:54 -0400

    Relocate vitest config into vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import autoprefixer from "autoprefixer";
 import cssnano from "cssnano";
 import tailwindcss from "tailwindcss";
 import { defineConfig } from "vite";
+import { configDefaults, coverageConfigDefaults } from "vitest/config";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -14,6 +15,24 @@ export default defineConfig({
   css: {
     postcss: {
       plugins: [autoprefixer(), tailwindcss(), cssnano()],
+    },
+  },
+  test: {
+    environment: "edge-runtime",
+    environmentMatchGlobs: [
+      ["convex/**", "edge-runtime"],
+      ["**", "jsdom"],
+    ],
+    server: { deps: { inline: ["convex-test"] } },
+    exclude: [...configDefaults.exclude],
+    coverage: {
+      reporter: ["text", "json-summary", "json"],
+      exclude: [
+        ...coverageConfigDefaults.exclude,
+        "**/*.config.?(c|m)[jt]s?(x)",
+        "convex/_generated/**",
+        "src/routeTree.gen.ts",
+      ],
     },
   },
 });
