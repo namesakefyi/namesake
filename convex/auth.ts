20f06247681f6db5bcca3b92e7a750af67e61538
Eva Decker <eva@namesake.fyi>
2024-09-17 18:51:37 -0400

    chore: Specify Resend auth key and "from" address (#67)

--- convex/auth.ts
+++ convex/auth.ts
@@ -2,5 +2,10 @@ import Resend from "@auth/core/providers/resend";
 import { convexAuth } from "@convex-dev/auth/server";
 
 export const { auth, signIn, signOut, store } = convexAuth({
-  providers: [Resend],
+  providers: [
+    Resend({
+      apiKey: process.env.AUTH_RESEND_KEY,
+      from: process.env.AUTH_EMAIL ?? "Namesake <no-reply@namesake.fyi>",
+    }),
+  ],
 });
