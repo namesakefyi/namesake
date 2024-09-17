540579ca9bfcd737c6c44c89d0bbdbd0266e1034
Eva Decker <itsevadecker@gmail.com>
2024-09-17 17:14:04 -0400

    Specify resend auth key

--- convex/auth.ts
+++ convex/auth.ts
@@ -2,5 +2,10 @@ import Resend from "@auth/core/providers/resend";
 import { convexAuth } from "@convex-dev/auth/server";
 
 export const { auth, signIn, signOut, store } = convexAuth({
-  providers: [Resend],
+  providers: [
+    Resend({
+      apiKey: process.env.AUTH_RESEND_KEY,
+      from: "no-reply@namesake.fyi",
+    }),
+  ],
 });
