7057f9e39abc28899ba947f16b6942b972dfa534
Eva Decker <itsevadecker@gmail.com>
2024-09-19 22:19:30 -0400

    I know how to write bash

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@
 # Deploy preview branches to Cloudflare Pages.
 # Production deploys are handled via `.github/workflows/deploy.yml`.
 
-if [ "$CF_PAGES_BRANCH" !== "main" ]; then
+if [ ! "$CF_PAGES_BRANCH" == "main" ]; then
   npx convex deploy --cmd "pnpm build" --preview-create "$CF_PAGES_BRANCH" --preview-run "seed"
 fi
