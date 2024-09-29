d7fa9391f6b7a7dd64814a3dda7f3aa26280105a
Eva Decker <itsevadecker@gmail.com>
2024-09-29 12:36:20 -0400

    Add comment

--- src/components/QuestStep/QuestStep.tsx
+++ src/components/QuestStep/QuestStep.tsx
@@ -65,6 +65,7 @@ export function QuestFields(props: { questFields: Doc<"questFields">[] }) {
     }
   };
 
+  // TODO: Add skeleton loaders
   return <>{props.questFields.map((field) => markupForField(field))}</>;
 }
 
