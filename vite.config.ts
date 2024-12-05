9f94fabc42fd3b38f55a4b11b24cad6e2ae373b7
Eva Decker <itsevadecker@gmail.com>
2024-12-04 22:01:43 -0500

    RelativeCI bundle comparison

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,30 @@ import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import react from "@vitejs/plugin-react-swc";
 import autoprefixer from "autoprefixer";
 import cssnano from "cssnano";
+import webpackStatsPlugin from "rollup-plugin-webpack-stats";
 import tailwindcss from "tailwindcss";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [TanStackRouterVite(), tsconfigPaths(), react()],
+  build: {
+    rollupOptions: {
+      output: {
+        // Use a supported file pattern for Vite 5/Rollup 4
+        // @doc https://relative-ci.com/documentation/guides/vite-config
+        assetFileNames: "assets/[name].[hash][extname]",
+        chunkFileNames: "assets/[name].[hash].js",
+        entryFileNames: "assets/[name].[hash].js",
+      },
+    },
+  },
+  plugins: [
+    TanStackRouterVite(),
+    tsconfigPaths(),
+    react(),
+    webpackStatsPlugin(),
+  ],
   define: {
     APP_VERSION: JSON.stringify(process.env.npm_package_version),
   },
