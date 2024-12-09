7f1e229b3d60317b12a4d13ebf35f190e0638a3b
Eva Decker <itsevadecker@gmail.com>
2024-12-08 23:08:55 -0500

    Formatting

--- convex/auth.test.ts
+++ convex/auth.test.ts
@@ -1,4 +1,3 @@
-import { faker } from "@faker-js/faker";
 import { convexTest } from "convex-test";
 import { describe, expect, it, vi } from "vitest";
 import { api } from "./_generated/api";
@@ -6,50 +5,46 @@ import { createOrUpdateUser } from "./auth";
 import schema from "./schema";
 import { modules } from "./test.setup";
 
-describe("auth roles", () => {
-  it("should set role to admin in development environment", async () => {
-    const t = convexTest(schema, modules);
+describe("auth", () => {
+  describe("createOrUpdateUser", () => {
+    it("should set role to admin in development environment", async () => {
+      const t = convexTest(schema, modules);
 
-    const email = faker.internet.email();
-
-    // Set NODE_ENV to development
-    vi.stubEnv("NODE_ENV", "development");
-    await t.run(async (ctx) => {
-      return await createOrUpdateUser(ctx, {
-        profile: {
-          email,
-        },
+      vi.stubEnv("NODE_ENV", "development");
+      await t.run(async (ctx) => {
+        return await createOrUpdateUser(ctx, {
+          profile: {
+            email: "devUser@test.com",
+          },
+        });
       });
-    });
 
-    const user = await t.query(api.users.getByEmail, {
-      email,
-    });
-
-    expect(user?.role).toBe("admin");
-    vi.unstubAllEnvs();
-  });
-
-  it("should set role to user in production environment", async () => {
-    const t = convexTest(schema, modules);
-
-    const email = faker.internet.email();
-
-    // Set NODE_ENV to production
-    vi.stubEnv("NODE_ENV", "production");
-    await t.run(async (ctx) => {
-      return await createOrUpdateUser(ctx, {
-        profile: {
-          email,
-        },
+      const user = await t.query(api.users.getByEmail, {
+        email: "devUser@test.com",
       });
+
+      expect(user?.role).toBe("admin");
+      vi.unstubAllEnvs();
     });
 
-    const user = await t.query(api.users.getByEmail, {
-      email,
-    });
+    it("should set role to user in production environment", async () => {
+      const t = convexTest(schema, modules);
 
-    expect(user?.role).toBe("user");
-    vi.unstubAllEnvs();
+      vi.stubEnv("NODE_ENV", "production");
+      await t.run(async (ctx) => {
+        return await createOrUpdateUser(ctx, {
+          profile: {
+            email: "prodUser@test.com",
+          },
+        });
+      });
+
+      const user = await t.query(api.users.getByEmail, {
+        email: "prodUser@test.com",
+      });
+
+      expect(user?.role).toBe("user");
+      vi.unstubAllEnvs();
+    });
   });
 });
