7ddc558baa33c7465979d5ebaea63b083d327d9f
mb bischoff <mb@mbbischoff.com>
2025-02-10 12:28:06 -0500

    fix: Removed unused span

--- src/components/TimeAgo.tsx
+++ src/components/TimeAgo.tsx
@@ -2,13 +2,8 @@ import RelativeTime from "react-relative-time";
 
 interface TimeAgoProps {
   date: Date;
-  className?: string;
 }
 
-export const TimeAgo: React.FC<TimeAgoProps> = ({ date, className }) => {
-  return (
-    <span className={className}>
-      <RelativeTime value={date} titleFormat="MM/DD/YYYY HH:mm" />
-    </span>
-  );
+export const TimeAgo: React.FC<TimeAgoProps> = ({ date }) => {
+  return <RelativeTime value={date} titleFormat="MM/DD/YYYY HH:mm" />;
 };
