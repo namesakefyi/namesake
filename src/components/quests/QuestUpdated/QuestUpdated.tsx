4426c3589b74e59bf912be3092d2c247b8a439f3
mb bischoff <mb@mbbischoff.com>
2025-02-11 13:43:43 -0500

    fix: Remove the Tooltip in the footer of Quests in favor of the native title tooltip

--- src/components/quests/QuestUpdated/QuestUpdated.tsx
+++ src/components/quests/QuestUpdated/QuestUpdated.tsx
@@ -1,28 +1,15 @@
-import { Button, Tooltip } from "@/components/common";
-import { TooltipTrigger } from "@/components/common/Tooltip/Tooltip";
+import { TimeAgo } from "@/components/TimeAgo";
 import type { Doc } from "@convex/_generated/dataModel";
-import RelativeTime from "react-relative-time";
 
 interface QuestUpdatedProps {
   quest: Doc<"quests">;
 }
 export function QuestUpdated({ quest }: QuestUpdatedProps) {
   const updatedTime = quest.updatedAt ? (
-    <RelativeTime value={new Date(quest.updatedAt)} />
+    <TimeAgo date={new Date(quest.updatedAt)} />
   ) : (
     "Never"
   );
 
-  return (
-    <TooltipTrigger>
-      <Button variant="ghost" className="text-gray-dim text-sm">
-        Updated {updatedTime}
-      </Button>
-      <Tooltip>
-        {quest.updatedAt
-          ? new Date(quest.updatedAt).toLocaleString()
-          : "Never updated"}
-      </Tooltip>
-    </TooltipTrigger>
-  );
+  return <span className="text-gray-dim text-sm">Updated {updatedTime}</span>;
 }
