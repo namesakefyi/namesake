af4ad688d7d6f51d83efd57516b0ac5b49692b78
Eva Decker <itsevadecker@gmail.com>
2025-03-11 18:48:46 -0400

    Naming

--- scripts/deleteConvexTables.sh
+++ scripts/deleteConvexTables.sh
@@ -1,1 +1,2 @@
+# https://stack.convex.dev/yolo-fast-mvp#delete-dev-data-liberally-and-maintain-a-seed-script-to-re-initialize
 for tableName in `npx convex data`; do npx convex import --table $tableName --replace -y --format jsonLines /dev/null; done
