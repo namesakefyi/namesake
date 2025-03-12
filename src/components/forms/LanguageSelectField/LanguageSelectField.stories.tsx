af07c51b31688082d4c1d1ed4d6bec4f21e93cf2
Eva Decker <itsevadecker@gmail.com>
2025-03-12 14:04:32 -0400

    Test coverage

---
+++ src/components/forms/LanguageSelectField/LanguageSelectField.stories.tsx
@@ -0,0 +1,28 @@
+import {
+  LanguageSelectField,
+  type LanguageSelectFieldProps,
+} from "@/components/forms";
+import type { Meta } from "@storybook/react";
+
+const meta: Meta<typeof LanguageSelectField> = {
+  component: LanguageSelectField,
+  parameters: {
+    layout: "padded",
+  },
+};
+
+export default meta;
+
+export const Example = (args: LanguageSelectFieldProps) => (
+  <LanguageSelectField {...args} />
+);
+
+Example.args = {
+  label: "Language",
+  name: "language",
+  children: (
+    <div className="border border-gray-dim p-3 rounded-lg text-gray-dim">
+      Children
+    </div>
+  ),
+};
