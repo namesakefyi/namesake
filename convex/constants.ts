66276bdb052f361186362aca2823695f25d39ff5
Eva Decker <itsevadecker@gmail.com>
2025-03-15 22:10:40 -0400

    Add comment

--- convex/constants.ts
+++ convex/constants.ts
@@ -337,4 +337,6 @@ export const COMMON_PRONOUNS = [
   "he/him/his",
 ];
 
+// Used for React-Aria's Selection.
+// https://react-spectrum.adobe.com/react-aria/selection.html#select-all
 export const ALL = "all";
