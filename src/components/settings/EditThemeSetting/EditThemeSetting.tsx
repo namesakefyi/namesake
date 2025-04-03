0e174842eb91f8e9523b4172535af962c3dda0b3
Eva Decker <itsevadecker@gmail.com>
2025-04-03 15:36:45 -0400

    SettingsItem responsive tweaks

--- src/components/settings/EditThemeSetting/EditThemeSetting.tsx
+++ src/components/settings/EditThemeSetting/EditThemeSetting.tsx
@@ -1,5 +1,4 @@
 import { ToggleButton, ToggleButtonGroup } from "@/components/common";
-import { SettingsItem } from "@/components/settings";
 import { useTheme } from "@/utils/useTheme";
 import { THEMES } from "@convex/constants";
 
@@ -7,19 +6,17 @@ export const EditThemeSetting = () => {
   const { themeSelection, setTheme } = useTheme();
 
   return (
-    <SettingsItem label="Theme">
-      <ToggleButtonGroup
-        selectionMode="single"
-        disallowEmptySelection
-        selectedKeys={themeSelection}
-        onSelectionChange={setTheme}
-      >
-        {Object.entries(THEMES).map(([theme, details]) => (
-          <ToggleButton key={theme} id={theme}>
-            {details.label}
-          </ToggleButton>
-        ))}
-      </ToggleButtonGroup>
-    </SettingsItem>
+    <ToggleButtonGroup
+      selectionMode="single"
+      disallowEmptySelection
+      selectedKeys={themeSelection}
+      onSelectionChange={setTheme}
+    >
+      {Object.entries(THEMES).map(([theme, details]) => (
+        <ToggleButton key={theme} id={theme}>
+          {details.label}
+        </ToggleButton>
+      ))}
+    </ToggleButtonGroup>
   );
 };
--- src/components/settings/SettingsItem/SettingsItem.tsx
+++ src/components/settings/SettingsItem/SettingsItem.tsx
@@ -11,7 +11,7 @@ export const SettingsItem = ({
   description,
   children,
 }: SettingsItemProps) => (
-  <div className="flex justify-between items-center gap-x-4 gap-y-3 min-h-16 py-3 px-4">
+  <div className="flex justify-between items-center gap-x-4 gap-y-3 min-h-16 py-3 px-4 border border-gray-dim first-of-type:rounded-t-lg last-of-type:rounded-b-lg not-first-of-type:border-t-0 not-last-of-type:border-b-gray-a3">
     <div>
       <Heading className="text-base -mt-px">{label}</Heading>
       {description && (
