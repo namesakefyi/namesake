6ef2a2e17eb8856bd25aca4101d436bedc8fedf8
Eva Decker <itsevadecker@gmail.com>
2025-04-12 18:05:48 -0400

    Toolbar section cleanup

--- src/components/common/Editor/EditorToolbar.tsx
+++ src/components/common/Editor/EditorToolbar.tsx
@@ -51,6 +51,23 @@ const EditorToggleButton = ({
   );
 };
 
+type EditorToolbarSectionProps = {
+  children: React.ReactNode;
+  isVisible?: boolean;
+};
+
+const EditorToolbarSection = ({
+  children,
+  isVisible = true,
+}: EditorToolbarSectionProps) => {
+  return isVisible ? (
+    <>
+      <Separator orientation="vertical" />
+      {children}
+    </>
+  ) : null;
+};
+
 type EditorToolbarProps = {
   editor: Editor;
   extensions?: ExtensionGroup[];
@@ -99,7 +116,7 @@ export const EditorToolbar = ({ editor, extensions }: EditorToolbarProps) => {
   return (
     <Toolbar
       orientation="horizontal"
-      className="p-3 shadow-[0_1px_0_0_var(--color-gray-a5)] sticky bg-element rounded-t-lg top-0 z-10"
+      className="px-3 py-2 shadow-[0_1px_0_0_var(--color-gray-a5)] sticky bg-element rounded-t-lg top-0 z-10"
     >
       <EditorToggleButton
         icon={Undo}
@@ -115,116 +132,99 @@ export const EditorToolbar = ({ editor, extensions }: EditorToolbarProps) => {
         isDisabled={!editor.can().chain().focus().redo().run()}
         isSelected={false}
       />
-
-      {extensions?.includes("headings") && (
-        <>
-          <Separator orientation="vertical" />
-          <EditorToggleButton
-            icon={Heading}
-            label="Heading"
-            onPress={() =>
-              editor.chain().focus().toggleHeading({ level: 2 }).run()
-            }
-            isDisabled={
-              !editor.can().chain().focus().toggleHeading({ level: 2 }).run()
-            }
-            isSelected={editor.isActive("heading", { level: 2 })}
-          />
-          <EditorToggleButton
-            icon={Heading2}
-            label="Subheading"
-            onPress={() =>
-              editor.chain().focus().toggleHeading({ level: 3 }).run()
-            }
-            isDisabled={
-              !editor.can().chain().focus().toggleHeading({ level: 3 }).run()
-            }
-            isSelected={editor.isActive("heading", { level: 3 })}
-          />
-        </>
-      )}
-      {extensions?.includes("basic") && (
-        <>
-          <Separator orientation="vertical" />
-          <EditorToggleButton
-            icon={BoldIcon}
-            label="Bold"
-            onPress={() => editor.chain().focus().toggleBold().run()}
-            isDisabled={!editor.can().chain().focus().toggleBold().run()}
-            isSelected={editor.isActive("bold")}
-          />
-          <EditorToggleButton
-            icon={ItalicIcon}
-            label="Italic"
-            onPress={() => editor.chain().focus().toggleItalic().run()}
-            isDisabled={!editor.can().chain().focus().toggleItalic().run()}
-            isSelected={editor.isActive("italic")}
-          />
-          <EditorToggleButton
-            icon={TextQuote}
-            label="Quote"
-            onPress={() => editor.chain().focus().toggleBlockquote().run()}
-            isDisabled={!editor.can().chain().focus().toggleBlockquote().run()}
-            isSelected={editor.isActive("blockquote")}
-          />
-          <EditorToggleButton
-            icon={Link}
-            label="Link"
-            onPress={setLink}
-            isDisabled={
-              !editor.can().chain().focus().setLink({ href: "" }).run()
-            }
-            isSelected={editor.isActive("link")}
-          />
-          <EditorToggleButton
-            icon={Unlink}
-            label="Unlink"
-            onPress={() => editor.chain().focus().unsetLink().run()}
-            isDisabled={!editor.can().chain().focus().unsetLink().run()}
-            isSelected={false}
-          />
-        </>
-      )}
-
-      {extensions?.includes("lists") && (
-        <>
-          <Separator orientation="vertical" />
-          <EditorToggleButton
-            icon={ListOrdered}
-            label="Numbered list"
-            onPress={() => editor.chain().focus().toggleOrderedList().run()}
-            isDisabled={!editor.can().chain().focus().toggleOrderedList().run()}
-            isSelected={editor.isActive("orderedList")}
-          />
-          <EditorToggleButton
-            icon={List}
-            label="Unordered list"
-            onPress={() => editor.chain().focus().toggleBulletList().run()}
-            isDisabled={!editor.can().chain().focus().toggleBulletList().run()}
-            isSelected={editor.isActive("bulletList")}
-          />
-        </>
-      )}
-
-      {extensions?.includes("advanced") && (
-        <>
-          <Separator orientation="vertical" />
-          <EditorToggleButton
-            icon={Milestone}
-            label="Steps"
-            onPress={() => editor.chain().focus().toggleSteps().run()}
-            isDisabled={!editor.can().chain().focus().toggleSteps().run()}
-            isSelected={editor.isActive("steps")}
-          />
-          <EditorToggleButton
-            icon={MousePointerClick}
-            label="Button"
-            onPress={() => editor.chain().focus().toggleButton().run()}
-            isDisabled={!editor.can().chain().focus().toggleButton().run()}
-            isSelected={editor.isActive("button")}
-          />
-        </>
-      )}
+      <EditorToolbarSection isVisible={extensions?.includes("headings")}>
+        <EditorToggleButton
+          icon={Heading}
+          label="Heading"
+          onPress={() =>
+            editor.chain().focus().toggleHeading({ level: 2 }).run()
+          }
+          isDisabled={
+            !editor.can().chain().focus().toggleHeading({ level: 2 }).run()
+          }
+          isSelected={editor.isActive("heading", { level: 2 })}
+        />
+        <EditorToggleButton
+          icon={Heading2}
+          label="Subheading"
+          onPress={() =>
+            editor.chain().focus().toggleHeading({ level: 3 }).run()
+          }
+          isDisabled={
+            !editor.can().chain().focus().toggleHeading({ level: 3 }).run()
+          }
+          isSelected={editor.isActive("heading", { level: 3 })}
+        />
+      </EditorToolbarSection>
+      <EditorToolbarSection isVisible={extensions?.includes("basic")}>
+        <EditorToggleButton
+          icon={BoldIcon}
+          label="Bold"
+          onPress={() => editor.chain().focus().toggleBold().run()}
+          isDisabled={!editor.can().chain().focus().toggleBold().run()}
+          isSelected={editor.isActive("bold")}
+        />
+        <EditorToggleButton
+          icon={ItalicIcon}
+          label="Italic"
+          onPress={() => editor.chain().focus().toggleItalic().run()}
+          isDisabled={!editor.can().chain().focus().toggleItalic().run()}
+          isSelected={editor.isActive("italic")}
+        />
+        <EditorToggleButton
+          icon={TextQuote}
+          label="Quote"
+          onPress={() => editor.chain().focus().toggleBlockquote().run()}
+          isDisabled={!editor.can().chain().focus().toggleBlockquote().run()}
+          isSelected={editor.isActive("blockquote")}
+        />
+        <EditorToggleButton
+          icon={Link}
+          label="Link"
+          onPress={setLink}
+          isDisabled={!editor.can().chain().focus().setLink({ href: "" }).run()}
+          isSelected={editor.isActive("link")}
+        />
+        <EditorToggleButton
+          icon={Unlink}
+          label="Unlink"
+          onPress={() => editor.chain().focus().unsetLink().run()}
+          isDisabled={!editor.can().chain().focus().unsetLink().run()}
+          isSelected={false}
+        />
+      </EditorToolbarSection>
+      <EditorToolbarSection isVisible={extensions?.includes("lists")}>
+        <EditorToggleButton
+          icon={ListOrdered}
+          label="Numbered list"
+          onPress={() => editor.chain().focus().toggleOrderedList().run()}
+          isDisabled={!editor.can().chain().focus().toggleOrderedList().run()}
+          isSelected={editor.isActive("orderedList")}
+        />
+        <EditorToggleButton
+          icon={List}
+          label="Unordered list"
+          onPress={() => editor.chain().focus().toggleBulletList().run()}
+          isDisabled={!editor.can().chain().focus().toggleBulletList().run()}
+          isSelected={editor.isActive("bulletList")}
+        />
+      </EditorToolbarSection>
+      <EditorToolbarSection isVisible={extensions?.includes("advanced")}>
+        <EditorToggleButton
+          icon={Milestone}
+          label="Steps"
+          onPress={() => editor.chain().focus().toggleSteps().run()}
+          isDisabled={!editor.can().chain().focus().toggleSteps().run()}
+          isSelected={editor.isActive("steps")}
+        />
+        <EditorToggleButton
+          icon={MousePointerClick}
+          label="Button"
+          onPress={() => editor.chain().focus().toggleButton().run()}
+          isDisabled={!editor.can().chain().focus().toggleButton().run()}
+          isSelected={editor.isActive("button")}
+        />
+      </EditorToolbarSection>
     </Toolbar>
   );
 };
