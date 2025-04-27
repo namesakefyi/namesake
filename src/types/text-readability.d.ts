0d8702bb14cb9d6ad88a8e680e241a363f528184
Eva Decker <itsevadecker@gmail.com>
2025-04-27 18:21:14 -0400

    Remove text-readability extension

--- src/types/text-readability.d.ts
+++
@@ -1,5 +0,0 @@
-declare module "text-readability" {
-  export function fleschReadingEase(text: string): number;
-  export function daleChallReadabilityScore(text: string): number;
-  export function textStandard(text: string, float_output?: boolean): string;
-}
