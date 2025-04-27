a35221bfa056725a38cb1144d2cd28e26d024bb6
Eva Decker <itsevadecker@gmail.com>
2025-04-27 11:44:20 -0400

    usePDFDetails

--- src/components/documents/DocumentsNav/DocumentsNav.tsx
+++ src/components/documents/DocumentsNav/DocumentsNav.tsx
@@ -1,34 +1,22 @@
 import { Nav, NavItem } from "@/components/common";
-import type { PDFDefinition } from "@/constants";
-import { getPdfDefinition } from "@/forms";
+import type { PDFId } from "@/constants";
+import { usePDFDetails } from "@/hooks/usePDFDetails";
 import { api } from "@convex/_generated/api";
 import { useQuery } from "convex/react";
 import { FileText } from "lucide-react";
-import { useEffect, useState } from "react";
 
 interface DocumentsNavProps {
   className?: string;
 }
 
 export const DocumentsNav = ({ className }: DocumentsNavProps) => {
-  const [pdfDefinitions, setPdfDefinitions] = useState<PDFDefinition[]>([]);
-  const pdfIds = useQuery(api.userDocuments.list);
-
-  useEffect(() => {
-    const fetchPdfDefinitions = async () => {
-      if (!pdfIds) return;
-
-      const pdfDefinitions = await Promise.all(
-        pdfIds.map((pdfId) => getPdfDefinition(pdfId.pdfId)),
-      );
-      setPdfDefinitions(pdfDefinitions);
-    };
-    fetchPdfDefinitions();
-  }, [pdfIds]);
+  const userDocuments = useQuery(api.userDocuments.list);
+  const pdfIds = userDocuments?.map((userDocument) => userDocument.pdfId);
+  const pdfs = usePDFDetails(pdfIds as PDFId[]);
 
   return (
     <Nav className={className}>
-      {pdfDefinitions?.map((pdf) => (
+      {pdfs?.map((pdf) => (
         <NavItem
           key={pdf.id}
           icon={FileText}
