50a17c5c9829aa624ff00ab408e4ee314966f629
Eva Decker <itsevadecker@gmail.com>
2025-05-21 16:49:20 -0400

    Easier linking of internal routes

--- src/components/common/Editor/extensions/ButtonComponent.tsx
+++ src/components/common/Editor/extensions/ButtonComponent.tsx
@@ -4,26 +4,40 @@ import {
   Form,
   Link,
   Popover,
+  Select,
+  SelectItem,
+  Tab,
+  TabList,
+  TabPanel,
+  Tabs,
   TextField,
   Tooltip,
   TooltipTrigger,
 } from "@/components/common";
+import { useRouter } from "@tanstack/react-router";
 import {
   NodeViewContent,
   type NodeViewProps,
   NodeViewWrapper,
 } from "@tiptap/react";
 import { LinkIcon, Unlink } from "lucide-react";
-import { useRef, useState } from "react";
+import { type Key, useRef, useState } from "react";
 import { useInteractOutside } from "react-aria";
 
 export default function ButtonComponent({ editor, node }: NodeViewProps) {
+  const { routesByPath } = useRouter();
   const [url, setUrl] = useState<string | null>(
     node.attrs.href && node.attrs.href.length > 0
       ? node.attrs.href
       : "https://",
   );
+  const [form, setForm] = useState<string | null>(
+    node.attrs.href?.startsWith("/forms/") ? node.attrs.href : null,
+  );
   const [isOpen, setIsOpen] = useState(false);
+  const [selectedTab, setSelectedTab] = useState<"namesake" | "external">(
+    node.attrs.href?.startsWith("/forms/") ? "namesake" : "external",
+  );
   const triggerRef = useRef<HTMLButtonElement>(null);
   const popoverRef = useRef<HTMLDivElement>(null);
   const containerRef = useRef<HTMLDivElement>(null);
@@ -34,7 +48,11 @@ export default function ButtonComponent({ editor, node }: NodeViewProps) {
   });
 
   const handleSave = () => {
-    editor.chain().focus().updateAttributes("button", { href: url }).run();
+    if (form) {
+      editor.chain().focus().updateAttributes("button", { href: form }).run();
+    } else if (url) {
+      editor.chain().focus().setLink({ href: url }).run();
+    }
   };
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
@@ -45,6 +63,10 @@ export default function ButtonComponent({ editor, node }: NodeViewProps) {
 
   const handleOpenChange = (isOpen: boolean) => {
     setIsOpen(isOpen);
+  };
+
+  const handleFormSelect = (value: Key) => {
+    setForm(value.toString());
   };
 
   return (
@@ -85,24 +107,67 @@ export default function ButtonComponent({ editor, node }: NodeViewProps) {
             shouldCloseOnInteractOutside={() => false} // Manually handling
             UNSTABLE_portalContainer={containerRef.current ?? undefined}
           >
-            <Form onSubmit={handleSubmit}>
-              <div className="flex flex-row items-start gap-1 w-full">
-                <TextField
-                  aria-label="URL"
-                  type="url"
-                  value={url ?? ""}
-                  onChange={setUrl}
-                  className="flex-1"
-                  prefix={
-                    <LinkIcon className="size-4 text-gray-9 ml-2 -mr-0.5" />
-                  }
-                  size="small"
-                />
-                <Button type="submit" variant="primary" size="small">
-                  Apply
-                </Button>
-              </div>
-            </Form>
+            <Tabs
+              selectedKey={selectedTab}
+              onSelectionChange={(key) =>
+                setSelectedTab(key as "namesake" | "external")
+              }
+              className="gap-2"
+            >
+              <TabList>
+                <Tab id="namesake">Namesake</Tab>
+                <Tab id="external">External</Tab>
+              </TabList>
+              <TabPanel id="namesake">
+                <Form
+                  onSubmit={handleSubmit}
+                  className="flex flex-row items-start gap-1 w-full"
+                >
+                  <Select
+                    aria-label="Select path"
+                    selectedKey={form}
+                    onSelectionChange={handleFormSelect}
+                    placeholder="Select path"
+                    className="flex-1"
+                    items={Object.entries(routesByPath)
+                      .filter(([path]) => path.startsWith("/forms/"))
+                      .map(([path]) => ({
+                        value: path,
+                        label: path,
+                      }))}
+                  >
+                    {(item) => (
+                      <SelectItem key={item.value} id={item.value}>
+                        {item.label}
+                      </SelectItem>
+                    )}
+                  </Select>
+                  <Button type="submit" variant="primary">
+                    Apply
+                  </Button>
+                </Form>
+              </TabPanel>
+              <TabPanel id="external">
+                <Form
+                  onSubmit={handleSubmit}
+                  className="flex flex-row items-start gap-1 w-full"
+                >
+                  <TextField
+                    aria-label="URL"
+                    type="url"
+                    value={url ?? ""}
+                    onChange={setUrl}
+                    className="flex-1"
+                    prefix={
+                      <LinkIcon className="size-4 text-gray-9 ml-2 -mr-0.5" />
+                    }
+                  />
+                  <Button type="submit" variant="primary">
+                    Apply
+                  </Button>
+                </Form>
+              </TabPanel>
+            </Tabs>
           </Popover>
         </div>
       ) : (
