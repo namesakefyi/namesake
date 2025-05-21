3ee94b3eb97b427563f5db73ef94f48a4ed4e4d2
Eva Decker <itsevadecker@gmail.com>
2025-05-21 12:19:39 -0400

    Add links to Executive Order and Lambda Legal

--- src/routes/_authenticated/forms/social-security.tsx
+++ src/routes/_authenticated/forms/social-security.tsx
@@ -244,11 +244,25 @@ function RouteComponent() {
             The gender marker is not shown on your Social Security card.
           </p>
           <p>
-            As of January 31, 2025, the Trump administration has directed the
-            Social Security Administration to stop processing gender marker
-            updates associated with social security records. If you apply for a
-            gender marker change with Social Security now, you will likely be
-            told that they cannot process your request.
+            As of January 20, 2025, the Trump administration has directed the
+            Social Security Administration to{" "}
+            <a
+              href="https://www.whitehouse.gov/presidential-actions/2025/01/defending-women-from-gender-ideology-extremism-and-restoring-biological-truth-to-the-federal-government/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              stop processing gender marker updates
+            </a>{" "}
+            associated with social security records. Per{" "}
+            <a
+              href="https://lambdalegal.org/tgnc-checklist-under-trump/#:~:text=If%20you%20apply%20for%20a%20gender%20marker%20change%20with%20Social%20Security%20now%2C%20you%20will%20likely%20be%20told%20that%20they%20cannot%20process%20your%20request."
+              target="_blank"
+              rel="noreferrer"
+            >
+              Lambda Legal
+            </a>
+            , if you apply for a gender marker change with Social Security now,
+            you will likely be told that they cannot process your request.
           </p>
         </Banner>
         <RadioGroupField
