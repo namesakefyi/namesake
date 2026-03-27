ea40636b2b499c12d97b79c190b61ad1fdac2352
Eva Decker <itsevadecker@gmail.com>
2026-03-27 19:33:59 -0400

    Maintain previous formatCurrency behavior

--- src/utils/formatCurrency.ts
+++ src/utils/formatCurrency.ts
@@ -1,9 +1,7 @@
 export const formatCurrency = (cost?: number | null) => {
-  if (!cost) return null;
-
   return Intl.NumberFormat("en-US", {
     style: "currency",
     currency: "USD",
     maximumFractionDigits: 0,
-  }).format(cost);
+  }).format(cost ?? 0);
 };
