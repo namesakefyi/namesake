2f29706ac777ddc93fe82abb2a4b01a42e6b8f9c
Eva Decker <itsevadecker@gmail.com>
2026-03-27 20:56:12 -0400

    Revert InputGroup changes

--- src/components/react/common/InputGroup/InputGroup.tsx
+++ src/components/react/common/InputGroup/InputGroup.tsx
@@ -7,20 +7,17 @@ import {
 } from "react-aria-components";
 import "./InputGroup.css";
 
-export interface InputGroupProps extends GroupProps {
+interface InputGroupProps extends GroupProps {
   label?: string;
 }
 
-export function InputGroup({ label, ...groupProps }: InputGroupProps) {
-  const labelId = useId();
+export function InputGroup(props: InputGroupProps) {
+  const id = useId();
   return (
     <div className="input-group">
-      {label ? <span id={labelId}>{label}</span> : null}
-      <Group
-        {...groupProps}
-        aria-labelledby={label ? labelId : groupProps["aria-labelledby"]}
-      >
-        {composeRenderProps(groupProps.children, (children, renderProps) => (
+      {props.label && <span id={id}>{props.label}</span>}
+      <Group {...props} aria-labelledby={id}>
+        {composeRenderProps(props.children, (children, renderProps) => (
           <InputContext.Provider value={{ disabled: renderProps.isDisabled }}>
             {children}
           </InputContext.Provider>
