c15b7a3e2eea1b45b64c00346b1e836512428076
Ky Decker <ky@namesake.fyi>
2026-04-16 13:56:02 -0400

    Use Atkinson Hyperlegible Soft (#480)

--- astro.config.ts
+++ astro.config.ts
@@ -3,7 +3,11 @@ import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
 import sanity from "@sanity/astro";
-import { defineConfig, passthroughImageService } from "astro/config";
+import {
+  defineConfig,
+  fontProviders,
+  passthroughImageService,
+} from "astro/config";
 import browserslist from "browserslist";
 import { browserslistToTargets } from "lightningcss";
 
@@ -35,6 +39,40 @@ export default defineConfig({
     // Eliminate trailing slashes from Cloudflare Pages
     // https://creativehike.com/posts/removing-trailng-slashes-astro
     format: "file",
+  },
+  experimental: {
+    fonts: [
+      {
+        provider: fontProviders.local(),
+        name: "Atkinson Hyperlegible Soft",
+        cssVariable: "--font-sans",
+        fallbacks: ["Helvetica", "Arial", "sans-serif"],
+        options: {
+          variants: [
+            {
+              weight: 400,
+              style: "normal",
+              src: ["./src/fonts/AtkinsonHyperlegibleSoft-Regular.woff2"],
+            },
+            {
+              weight: 400,
+              style: "italic",
+              src: ["./src/fonts/AtkinsonHyperlegibleSoft-RegularItalic.woff2"],
+            },
+            {
+              weight: 700,
+              style: "normal",
+              src: ["./src/fonts/AtkinsonHyperlegibleSoft-Bold.woff2"],
+            },
+            {
+              weight: 700,
+              style: "italic",
+              src: ["./src/fonts/AtkinsonHyperlegibleSoft-BoldItalic.woff2"],
+            },
+          ],
+        },
+      },
+    ],
   },
   devToolbar: {
     enabled: false,
