13aee8189ea13fc1e57182f000e20ad191f87c49
Ky Decker <itsevadecker@gmail.com>
2026-06-07 13:49:32 -0400

    Fix type error

--- src/pages/blog/[slug].png.ts
+++ src/pages/blog/[slug].png.ts
@@ -6,11 +6,14 @@ import { createOgImageResponse } from "../../utils/createOgImageResponse";
 
 export const getStaticPaths: GetStaticPaths = async () => {
   const posts = await getCollection("posts");
-  return posts.map((post) => ({ params: { slug: post.id }, props: { post } }));
+  return posts.map((post: CollectionEntry<"posts">) => ({
+    params: { slug: post.id },
+    props: { post },
+  }));
 };
 
 export const GET: APIRoute = async ({ props, request }) => {
-  const { post } = props as { post: CollectionEntry<"posts"> };
+  const { post }: { post: CollectionEntry<"posts"> } = props;
 
   return await createOgImageResponse({
     subhead: "Blog",
