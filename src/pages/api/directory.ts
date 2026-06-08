f98d5ef8ea32d708884f64418d543390ee8bfeb4
Ky Decker <itsevadecker@gmail.com>
2026-06-07 22:06:24 -0400

    Replace React directory components with inline scripts and Astro

--- src/pages/api/directory.ts
+++
@@ -1,9 +0,0 @@
-import type { APIRoute } from "astro";
-import { parseDirectorySearchParams } from "../../components/directory/DirectoryList/DirectoryList";
-import { getDirectoryContacts } from "../../utils/getDirectoryContacts";
-
-export const GET: APIRoute = async ({ url }) => {
-  const filters = parseDirectorySearchParams(url.searchParams);
-  const { contacts } = await getDirectoryContacts(filters);
-  return Response.json({ contacts });
-};
