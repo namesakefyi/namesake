5f88fdffe4299565b2fce5db5bd5924ff461f179
Ky Decker <itsevadecker@gmail.com>
2026-06-09 08:48:06 -0400

    Remove file format from config

--- astro.config.ts
+++ astro.config.ts
@@ -22,11 +22,6 @@ export default defineConfig({
   integrations: [sitemap(), mdx(), react()],
   prefetch: true,
   trailingSlash: "never",
-  build: {
-    // Eliminate trailing slashes from Cloudflare Pages
-    // https://creativehike.com/posts/removing-trailng-slashes-astro
-    format: "file",
-  },
   fonts: [
     {
       provider: fontProviders.local(),
