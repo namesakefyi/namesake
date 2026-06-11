a60da37f5d86366445314f32f6521a891da51e13
Ky Decker <itsevadecker@gmail.com>
2026-06-11 13:10:29 -0400

    Refactor query into new content/queries folder

--- src/constants/guides.ts
+++ src/constants/guides.ts
@@ -3,8 +3,3 @@ export const GUIDE_CATEGORY_ORDER = [
   "state-id",
   "birth-certificate",
 ];
-
-export const categoryRank = (id: string) => {
-  const i = GUIDE_CATEGORY_ORDER.indexOf(id);
-  return i === -1 ? GUIDE_CATEGORY_ORDER.length : i;
-};
