2d1764a76d95e4c9abe0218d1cab7d1b8b978bc4
Matthew Young <radicalmatt@fastmail.com>
2026-06-16 08:57:28 -0500

    Update remark-modified-time.mjs to use git's AuthorDate
    Changes the remark-modified-time plugin to use git's AuthorDate instead of the CommitterDate. AuthorDate should more accurately reflect the true "last modified" time where committer date represents the time of the last git action involving that file.

--- scripts/remark-modified-time.mjs
+++ scripts/remark-modified-time.mjs
@@ -3,7 +3,7 @@ import { execSync } from "node:child_process";
 export function remarkModifiedTime() {
   return (_tree, file) => {
     const filepath = file.history[0];
-    const result = execSync(`git log -1 --pretty="format:%cI" "${filepath}"`);
+    const result = execSync(`git log -1 --pretty="format:%aI" "${filepath}"`);
     file.data.astro.frontmatter.lastModified = result.toString();
   };
 }
