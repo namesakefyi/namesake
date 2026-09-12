6d76323027b41969b994a53eddae88362ea0396d
Ky Decker <itsevadecker@gmail.com>
2026-09-12 17:34:48 -0400

    Update comment

--- web/astro.config.ts
+++ web/astro.config.ts
@@ -92,9 +92,9 @@ export default defineConfig({
     },
     build: {
       cssMinify: "lightningcss",
-      // Prevent Vite from bundling FormContainer.css with otehr JS
-      // used when downloading forms. Was causing 404 /
-      // download failure on first attempt.
+      // Prevent Vite attempting to fetch FormContainer.css
+      // during form downloads. Downloads were previously
+      // failing on 1st try due to how Vite bundles code.
       cssCodeSplit: false,
     },
   },
